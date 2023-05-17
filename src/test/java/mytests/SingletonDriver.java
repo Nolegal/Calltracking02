4daaf2cd8fa0ece9230e3ea678060cb1241c6cba
Nolegal <pike1988@i.ua>
2023-05-17 15:22:15 +0300

    Press test2 commit

--- src/test/java/mytests/SingletonDriver.java
+++ src/test/java/mytests/SingletonDriver.java
@@ -2,6 +2,8 @@ package mytests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class SingletonDriver {
     private static WebDriver driver;
 
@@ -9,8 +11,15 @@ public class SingletonDriver {
 
     public static WebDriver getSingletonInstance() {
         if (null == driver) {
-           System.setProperty("webdriver.chrome.driver", ".//src/main/driver/chromedriver");
-                   driver = new ChromeDriver();
+          // System.setProperty("webdriver.chrome.driver", ".//src/main/driver/chromedriver");
+              //     driver = new ChromeDriver();
+
+            System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+            WebDriver driver;
+
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("headless");
+            driver= new ChromeDriver(options);
         }
         return driver;
     }
