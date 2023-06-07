9d2109fce46b63577b19e4a1d3768598e68234a8
Nolegal <pike1988@i.ua>
2023-06-07 15:33:13 +0300

    Press test12 commit

--- src/test/java/mytests/CallTracking.java
+++ src/test/java/mytests/CallTracking.java
@@ -1,163 +1,140 @@
 package mytests;
-
 import org.openqa.selenium.By;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
 
-public class CallTracking  extends BaseClass{
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+public class CallTracking {
+
+
+    @Test
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", ".//driver/chromedriver");
+
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
 
 
 
-    public CallTracking(){
-      super();
+
+        // System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+
+
+      // ChromeOptions options = new ChromeOptions();
+      // options.addArguments("headless");
+      // driver = new ChromeDriver(options);
+
+        driver.get("https://calltracking.ru/testqa/index.php?utm_source=google&utm_medium=cpc");
+        Thread.sleep(3000);
+        WebElement phone= driver.findElement(By.xpath("/html[1]/body[1]/div[1]/header[1]/div[2]/a[1]"));
+        String text= phone.getText();
+        System.out.println(text);
+
+
+        if (text.contains("+7 (495) 367-29-93")) {
+            System.out.println("number is changed 1st");
+        }
+         else if (text.contains("+7 (499) 558-58-47")) {
+            System.out.println("number is changed 2nd");
+        }
+        else if (text.contains("+7 (499) 406-17-63")) {
+            System.out.println("number is changed 3rd");
+        }
+        else {
+          System.out.println(text+"calltracking not working");
+        }
+
+        WebElement email= driver.findElement(By.xpath("/html[1]/body[1]/div[1]/main[1]/div[1]/aside[1]"));
+        String mail= email.getText();
+        System.out.println(mail);
+
+        if (mail.contains("test.email.v114+")){
+            System.out.println("email is changed");
+        }
+        else {
+            System.out.println(text+"emailtracking not working");
+        }
+
+        ((JavascriptExecutor) driver).executeScript("window.open()");
+        ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
+        driver.switchTo().window(tabs.get(1));
+        driver.get("https://calltracking.ru/testqa/index.php?utm_source=google&utm_medium=organic");
+
+        WebElement phone2= driver.findElement(By.xpath("/html[1]/body[1]/div[1]/header[1]/div[2]/a[1]"));
+        String text2= phone2.getText();
+        System.out.println(text2);
+
+
+        if (text2.contains("+7 (499) 558-35-93")) {
+            System.out.println("number 2 is changed");
+        }
+
+        else {
+            System.out.println(text2+"calltracking not working");
+        }
+
+        WebElement email2= driver.findElement(By.xpath("/html[1]/body[1]/div[1]/main[1]/div[1]/aside[1]"));
+        String mail2= email2.getText();
+        System.out.println(mail2);
+
+        if (mail2.contains("test.email.v114+")){
+            System.out.println("email2 is changed");
+        }
+        else {
+            System.out.println(text+"emailtracking not working");
+        }
+
+        ((JavascriptExecutor) driver).executeScript("window.open()");
+        ArrayList<String> tabs2 = new ArrayList<String>(driver.getWindowHandles());
+        driver.switchTo().window(tabs2.get(2));
+        driver.get("https://calltracking.ru/testqa/index.php?utm_source=google&utm_medium=anything");
+
+        WebElement phone3= driver.findElement(By.xpath("/html[1]/body[1]/div[1]/header[1]/div[2]/a[1]"));
+        String text3= phone3.getText();
+        System.out.println(text3);
+
+
+        if (text3.contains("+7 (495) 357-05-96")) {
+            System.out.println("number 3 is same");
+        }
+
+        else {
+            System.out.println(text3+"calltracking is hucked");
+        }
+
+        WebElement email3= driver.findElement(By.xpath("/html[1]/body[1]/div[1]/main[1]/div[1]/aside[1]"));
+        String mail3= email3.getText();
+        System.out.println(mail3);
+
+        if (mail3.contains("test.email.v114+")){
+            System.out.println("email3 is changed");
+        }
+        else {
+            System.out.println(text3+"emailtracking not working");
+        }
+
+        /*((JavascriptExecutor) driver).executeScript("window.open()");
+        ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
+        driver.switchTo().window(tabs.get(1));
+        driver.get("https://calltracking.ru/testqa/index.php?utm_source=google&utm_medium=organic");
+        ((JavascriptExecutor) driver).executeScript("window.open()");
+        ArrayList<String> tabs2 = new ArrayList<String>(driver.getWindowHandles());
+        driver.switchTo().window(tabs2.get(2));
+        driver.get("https://calltracking.ru/testqa/index.php?utm_source=google&utm_medium=cpc"); */
+
 
     }
 
-    @AfterClass public static void closeBrowser(){
-     driver.quit();
 
-      driver=null;
-    }
-
-
-    @Test(dataProvider ="testdata")
-
-    public static void openBrowser(String name,String email,String phone,String text){
-       navigateToURL("https://calltracking.ru/testqa/index.php?utm_source=google&utm_medium=anything");
-        JavascriptExecutor jse=(JavascriptExecutor) driver;
-        jse.executeScript("window.scrollBy(0,400)");
-
-        driver.findElement(By.xpath("//body/div[1]/form[1]/input[1]")).click();
-        driver.findElement(By.xpath("//body/div[1]/form[1]/input[1]")).sendKeys(name);
-
-        driver.findElement(By.xpath("//body/div[1]/form[1]/input[2]")).click();
-        driver.findElement(By.xpath("//body/div[1]/form[1]/input[2]")).sendKeys(email);
-
-        driver.findElement(By.xpath("//body/div[1]/form[1]/input[3]")).click();
-        driver.findElement(By.xpath("//body/div[1]/form[1]/input[3]")).sendKeys(phone);
-
-        driver.findElement(By.xpath("//body/div[1]/form[1]/textarea[1]")).click();
-        driver.findElement(By.xpath("//body/div[1]/form[1]/textarea[1]")).sendKeys(text);
-
-
-
-
-        driver.findElement (By.xpath ("//body/div[1]/form[1]/input[4]")).click();
-
-
-
-    }
-
-    @DataProvider(name="testdata")
-    public Object[][] TestDataFeed(){
-
-// Create object array with 2 rows and 2 column- first parameter is row and second is //column
-        Object [][] calltrackingdata=new Object[10][4];
-
-// Enter data to row 0 column 0
-        calltrackingdata[0][0]="Тест";
-// Enter data to row 0 column 1
-        calltrackingdata[0][1]="test@i.ru";
-// Enter data to row 1 column 0
-        calltrackingdata[0][2]="+389623920000";
-
-        calltrackingdata[0][3]="Нет";
-
-
-        calltrackingdata[1][0]="Тест";
-// Enter data to row 1 column 0
-        calltrackingdata[1][1]="test@i.ru";
-// return arrayobject to testscript
-        calltrackingdata[1][2]="+389623923921";
-
-        calltrackingdata[1][3]="Да";
-
-
-        calltrackingdata[2][0]="Тест";
-// Enter data to row 1 column 0
-        calltrackingdata[2][1]="test@i.ru";
-// return arrayobject to testscript
-        calltrackingdata[2][2]="+389623920002";
-
-        calltrackingdata[2][3]="Да";
-
-
-        calltrackingdata[3][0]="Тест";
-// Enter data to row 1 column 0
-        calltrackingdata[3][1]="test@i.ru";
-// return arrayobject to testscript
-        calltrackingdata[3][2]="+389623920003";
-
-        calltrackingdata[3][3]="Да";
-
-
-        calltrackingdata[4][0]="Тест";
-// Enter data to row 1 column 0
-        calltrackingdata[4][1]="test@i.ru";
-// return arrayobject to testscript
-        calltrackingdata[4][2]="+389623920004";
-
-        calltrackingdata[4][3]="Да";
-
-
-        calltrackingdata[5][0]="Тест";
-// Enter data to row 1 column 0
-        calltrackingdata[5][1]="test@i.ru";
-// return arrayobject to testscript
-        calltrackingdata[5][2]="+389623920005";
-
-        calltrackingdata[5][3]="Да";
-
-
-        calltrackingdata[6][0]="Тест";
-// Enter data to row 1 column 0
-        calltrackingdata[6][1]="test@i.ru";
-// return arrayobject to testscript
-        calltrackingdata[6][2]="+389623920006";
-
-        calltrackingdata[6][3]="Да";
-
-        calltrackingdata[7][0]="Тест";
-// Enter data to row 1 column 0
-        calltrackingdata[7][1]="test@i.ru";
-// return arrayobject to testscript
-        calltrackingdata[7][2]="+389623920007";
-
-        calltrackingdata[7][3]="Да";
-
-
-        calltrackingdata[8][0]="Тест";
-// Enter data to row 1 column 0
-        calltrackingdata[8][1]="test@i.ru";
-// return arrayobject to testscript
-        calltrackingdata[8][2]="+389623920008";
-
-        calltrackingdata[8][3]="Да";
-
-
-        calltrackingdata[9][0]="Тест";
-// Enter data to row 1 column 0
-        calltrackingdata[9][1]="test@i.ru";
-// return arrayobject to testscript
-        calltrackingdata[9][2]="+389623920009";
-
-        calltrackingdata[9][3]="Да";
-
-
-
-
-
-
-
-
-
-
-
-
-        return calltrackingdata;
-    }
 }
-
-
