ee93c3320e3a56a3dbcd63704d3d3298b9739e35
Nolegal <pike1988@i.ua>
2023-06-27 12:51:50 +0300

    Press test22 commit

--- src/test/java/mytests/CallTracking.java
+++ src/test/java/mytests/CallTracking.java
@@ -11,6 +11,8 @@ import org.testng.Reporter;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 import org.apache.log4j.PropertyConfigurator;
+
+
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
@@ -26,7 +28,7 @@ public class CallTracking {
 
         PropertyConfigurator.configure(log4jConfPath);
 
-  /*      System.setProperty("webdriver.chrome.driver", ".//driver/chromedriver");
+        System.setProperty("webdriver.chrome.driver", ".//driver/chromedriver");
 
           WebDriver driver=new ChromeDriver();
 
@@ -35,52 +37,52 @@ public class CallTracking {
         driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-*/
+
 
 
 
         System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 
-        WebDriver driver;
+       // WebDriver driver;
 
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("headless");
-        driver = new ChromeDriver(options);
+        //ChromeOptions options = new ChromeOptions();
+        //options.addArguments("headless");
+       // driver = new ChromeDriver(options);
 
         driver.get("https://calltracking.ru/testqa/index.php?utm_source=google&utm_medium=cpc");
         Thread.sleep(3000);
         WebElement phone= driver.findElement(By.xpath("/html[1]/body[1]/div[1]/header[1]/div[2]/a[1]"));
         String text= phone.getText();
-       System.out.println(text);
+      // System.out.println(text);
         log.info(text);
 
         if (text.contains("+7 (495) 367-29-93")) {
-            System.out.println("number is changed 1st");
+          //  System.out.println("number is changed 1st");
             log.info("number is changed 1st");
         }
          else if (text.contains("+7 (499) 558-58-47")) {
-            System.out.println("number is changed 2nd");
+           // System.out.println("number is changed 2nd");
             log.info("number is changed 2nd");
         }
         else if (text.contains("+7 (499) 406-17-63")) {
-            System.out.println("number is changed 3rd");
+           // System.out.println("number is changed 3rd");
             log.info("number is changed 3rd");
         }
         else {
-            System.out.println(text+"calltracking not working");
+           // System.out.println(text+"calltracking not working");
            log.info(text+"calltracking not working");
         }
 
         WebElement email= driver.findElement(By.xpath("/html[1]/body[1]/div[1]/main[1]/div[1]/aside[1]"));
         String mail= email.getText();
-        System.out.println(mail);
+        //System.out.println(mail);
         log.info(mail);
         if (mail.contains("test.email.v114+")){
-            System.out.println("email is changed");
+           // System.out.println("email is changed");
             log.info("email is changed");
         }
         else {
-            System.out.println(mail+"emailtracking not working");
+          //  System.out.println(mail+"emailtracking not working");
             log.info(mail+"emailtracking not working");
         }
 
@@ -91,29 +93,29 @@ public class CallTracking {
 
         WebElement phone2= driver.findElement(By.xpath("/html[1]/body[1]/div[1]/header[1]/div[2]/a[1]"));
         String text2= phone2.getText();
-        System.out.println(text2);
+       // System.out.println(text2);
         log.info(text2);
 
         if (text2.contains("+7 (499) 558-35-93")) {
-            System.out.println("number 2 is changed");
+           // System.out.println("number 2 is changed");
             log.info("number 2 is changed");
         }
 
         else {
-            System.out.println(text2+"calltracking not working");
+           // System.out.println(text2+"calltracking not working");
             log.info(text2+"calltracking not working");
         }
 
         WebElement email2= driver.findElement(By.xpath("/html[1]/body[1]/div[1]/main[1]/div[1]/aside[1]"));
         String mail2= email2.getText();
-        System.out.println(mail2);
+        //System.out.println(mail2);
         log.info(mail2);
         if (mail2.contains("test.email.v114+")){
-            System.out.println("email2 is changed");
+          //  System.out.println("email2 is changed");
             log.info("email2 is changed");
         }
         else {
-            System.out.println(mail2+"emailtracking not working");
+           // System.out.println(mail2+"emailtracking not working");
             log.info(mail2+"emailtracking not working");
         }
 
@@ -124,30 +126,30 @@ public class CallTracking {
 
         WebElement phone3= driver.findElement(By.xpath("/html[1]/body[1]/div[1]/header[1]/div[2]/a[1]"));
         String text3= phone3.getText();
-        System.out.println(text3);
+        //System.out.println(text3);
         log.info(text3);
 
         if (text3.contains("+7 (495) 357-05-96")) {
-            System.out.println("number 3 is same");
+           // System.out.println("number 3 is same");
             log.info("number 3 is same");
         }
 
         else {
-            System.out.println(text3+"calltracking is hucked");
+            //System.out.println(text3+"calltracking is hucked");
             log.info(text3+"calltracking is hucked");
         }
 
         WebElement email3= driver.findElement(By.xpath("/html[1]/body[1]/div[1]/main[1]/div[1]/aside[1]"));
         String mail3= email3.getText();
-        System.out.println(mail3);
+        //System.out.println(mail3);
         log.info(mail3);
 
         if (mail3.contains("test.email.v114+")){
-            System.out.println("email3 is changed");
+           // System.out.println("email3 is changed");
             log.info("email3 is changed");
         }
         else {
-            System.out.println(mail3+"emailtracking not working");
+           // System.out.println(mail3+"emailtracking not working");
             log.info(mail3+"emailtracking not working");
         }
 
